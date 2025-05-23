43d0885a1d954a05bd3ccd87158b0c9eb8b7cc11
gvielza <voltagnelly@gmail.com>
2025-05-23 20:43:37 -0300

    CRUD

--- base_datos/conexion.py
+++ base_datos/conexion.py
@@ -1,11 +1,42 @@
 import sqlite3
 
 class Conexion:
-    def __init__(self, path_bd):
+    def __init__(self, path_bd="base_datos/mi_bd.db"):
         self.conexion=sqlite3.connect(path_bd)
         self.cursor=self.conexion.cursor()
         self.crear_tabla_usuario()
 
 
-    def crea_tabla_usuario(self):
-        
+    def crear_tabla_usuario(self):
+        self.cursor.execute('''
+CREATE TABLE IF NOT EXISTS usuarios
+               (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               nombre TEXT,
+               usuario TEXT,
+               correo TEXT,
+               rol TEXT
+               )
+''')   
+        self.conexion.commit()
+
+    def insertar_usuario(self,nombre, usuario,correo,rol):
+        self.cursor.execute('''
+               INSERT INTO usuarios (
+               nombre,usuario,correo,rol
+               )
+               VALUES(?, ?, ?, ?)
+                ''',(nombre,usuario,correo,rol))
+        self.conexion.commit()
+
+    def mostrar_usuarios(self):
+        self.cursor.execute("SELECT * from usuarios")
+        usuarios=self.cursor.fetchall()
+        return usuarios
+    
+    def eliminar_usuario(self,id):
+        self.cursor.execute("DELETE FROM usuarios WHERE id=?",(id,))
+        self.conexion.commit()
+    
+    def cerrar_conexion(self):
+        self.conexion.close()
--- usuario.py
+++ usuario.py
@@ -1,1 +1,12 @@
 from base_datos.conexion import Conexion
+
+mi_conexion=Conexion()
+
+#mi_conexion.insertar_usuario("Gionnelly","gvielza","gionnelly.vielza@davinci.edu.ar","user")
+
+
+users=mi_conexion.mostrar_usuarios()
+#mi_conexion.eliminar_usuario(4)
+
+for usuario in users:
+    print(usuario)
